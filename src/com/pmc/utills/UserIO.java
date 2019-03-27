1ff29ea53877d33a59ee0a67aaf6030b64082f96
6uuuuuu <1051543710@qq.com>
2019-03-27 23:32:36 +0800

    third commit -- login completed

--- src/com/pmc/utills/UserIO.java
+++ src/com/pmc/utills/UserIO.java
@@ -7,38 +7,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserIO {
-    private static List<User> users= new ArrayList<>();
+    private static List<User> users = new ArrayList<>();
     private static final String USER_FILE = "user.obj";
 
     //写入用户信息
-    public boolean writeUsers(User user){
+    public boolean writeUsers(User user) throws BusinessException {
         try {
             ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(USER_FILE));
             oos.writeObject(users);
             oos.close();
             return true;
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new BusinessException("io.write.error");
         }
-        return false;
     }
+
     //读取用户信息
-    public boolean readUsers(User user){
+    public boolean readUsers() throws BusinessException {
         try {
             ObjectInputStream ois = new ObjectInputStream(new FileInputStream(USER_FILE));
             users = (List<User>) ois.readObject();//users为类的静态变量，因此该方法不需要返回一个List<User>的值
             ois.close();
             return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new BusinessException("io.read.error");
         }
-        return false;
     }
+
     //添加用户信息
-    public void addUsers(User user){
-        user.setId(users.size()+1);//设置用户Id每次加一
+    public void addUsers(User user) {
+        user.setId(users.size() + 1);//设置用户Id每次加一
         users.add(user);//每次新注册一个用户后，将该用户的信息加入users这个List中
     }
+
+    //查找用户信息
+    public boolean findUsers(User user) {
+        for (User u : users) {
+            if ((u.getUsername().equals(user.getUsername()))&&(u.getPassword().equals(user.getPassword()))) {
+                return true;
+            }
+        }return false;
+    }
+
+
 }
